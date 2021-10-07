069a4a64a80d46b69469de18ab1c6692555b22da
Mateo <mateo.saez@outlook.es>
2021-10-07 12:10:56 +0200

    Initial Commit

---
+++ mySFTP.py
@@ -0,0 +1,27 @@
+import pysftp
+import io
+
+class Connection(pysftp.Connection):
+
+    def __init__(self, host, username, pwd):
+        super().__init__(host, username=username,password=pwd)
+        self.file_names = []
+        self.dir_names = []
+        self.un_name = []
+        self.walktree("", self._store_files_name, self._store_dir_name, self._store_other_file_types)
+
+    def _store_files_name(self, fname):
+        self.file_names.append(fname) 
+
+    def _store_dir_name(self, dirname):
+        self.dir_names.append(dirname)
+
+    def _store_other_file_types(self, name):
+        self.un_name.append(name)
+
+    def get_file(self, f_name):
+        bf = io.BytesIO()
+        self.getfo(f_name, bf)
+        bf.seek(0)
+        return bf
+    
